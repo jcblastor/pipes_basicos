0cf61299e161eff3103e02bd1fbdb4cf7f835d9d
J. Carlos Guzmán <jcguzmanblack@gmail.com>
2023-01-06 21:58:10 -0300

    Pipes Basicos de angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesApp';
+  nombre: string = 'carlos guzmán';
+  valor: number = 1000;
+  obj = { nombre: 'Olga' }
+
+  mostrarNombre() {
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRouterModule } from './app-router.module';
+import { VentasModule } from './ventas/ventas.module';
+
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+
+// cambiar a locale la app
+import localeES from '@angular/common/locales/es-AR';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeES);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouterModule,
+    SharedModule,
+    VentasModule,
   ],
-  providers: [],
+  providers: [
+    // date-locale para usarlo de forma predetermina lo agregamos aca:
+    { provide: LOCALE_ID, useValue: 'es-AR' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
